859047bd724061917ede4f44e40f19c2508589dd
taylla <tayllagarcia1023@gmail.com>
2022-06-22 17:05:40 -0400

    add nome da equipe na tela inicial

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aplicativo-prog3-2bim</Text>
+
       <StatusBar style="auto" />
     </View>
   );
